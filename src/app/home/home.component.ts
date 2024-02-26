d328ca3638fc999bbb644e812be82e1d0c1be76a
uakubue <uakubue@gmail.com>
2024-02-26 16:23:54 +0100

    Added menu contents

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Food } from '../shared/models/Food';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  foods:Food[] = [];
+
+  constructor(private foodService: FoodService){}
+
+  ngOnInit(): void {
+    this.foods = this.foodService.getAll();
+  }
 }
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Food } from '../../shared/models/Food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll(): Food[]{
+    return [
+      {
+        id: 1,
+        name:"French fries",
+        price: 2.44,
+        tags: ['Fast Food', 'Fry'],
+        favourite: false,
+        stars: 2,
+        imageUrl: '/assets/images/foodie1.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      },
+
+      {
+        id: 2,
+        name:"Grilled Chicken",
+        price: 7.99,
+        tags: ['Fast Food', 'Fry'],
+        favourite: false,
+        stars: 2,
+        imageUrl: '/assets/images/foodie2.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      },
+
+      {
+        id: 3,
+        name:"Salad",
+        price: 1.86,
+        tags: ['Fast Food', 'Fry'],
+        favourite: false,
+        stars: 2,
+        imageUrl: '/assets/images/foodie3.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      },
+
+      {
+        id: 4,
+        name:"Italian Sushi",
+        price: 2.44,
+        tags: ['Fast Food', 'Fry'],
+        favourite: false,
+        stars: 2,
+        imageUrl: '/assets/images/foodie4.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      },
+
+      {
+        id: 5,
+        name:"Spaghetti bolonise",
+        price: 2.44,
+        tags: ['Fast Food', 'Fry'],
+        favourite: true,
+        stars: 24,
+        imageUrl: '/assets/images/foodie5.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      },
+
+      {
+        id: 6,
+        name:"Pizza",
+        price: 12.44,
+        tags: ['Fast Food', 'Fry'],
+        favourite: true,
+        stars: 4,
+        imageUrl: '/assets/images/foodie6.jpg',
+        origins: ['India', 'Asia'],
+        cookTime: '40 - 50min'
+      }
+    ]
+  }
+
+}
